5f7076b543a37ed255b4dd270adb6092342d28c1
chris <chrismaifeld@gmail.com>
2020-08-15 11:00:38 +0200

    Styling für verschiedene auflösungen optimiert.
    Styling für verschiedene auflösungen optimiert.

--- Versandtracks0rBlazor/Program.cs
+++ Versandtracks0rBlazor/Program.cs
@@ -17,7 +17,7 @@ namespace Versandtracks0rBlazor
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("app");
-    
+
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
             builder.Services.AddLogging(builder => builder
                     .AddBrowserConsole()
--- Versandtracks0rBlazor/wwwroot/JS/map.js
+++ Versandtracks0rBlazor/wwwroot/JS/map.js
@@ -1,86 +1,103 @@
-
 var map;
 
 function createLeafletMap(id) {
-    map = L.map(id).setView([0, 0], 12);
-    L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png').addTo(map);
+  map = L.map(id).setView([0, 0], 12);
+  L.tileLayer("http://{s}.tile.osm.org/{z}/{x}/{y}.png").addTo(map);
 }
 
 function showPath(history) {
-    console.log(history);
-    const points = history.map(e => L.latLng(e.lat, e.long));
-    const mapItems = [];
-    map.fitBounds(L.latLngBounds(points));
+  console.log(history);
+  const points = history.map((e) => L.latLng(e.lat, e.long));
+  const mapItems = [];
+  map.fitBounds(L.latLngBounds(points));
 
-    // checken was ist wenn 0 drin ist
-    //const path = L.polyline(points, { className: 'tracking_line', snakingSpeed: 500 });
+  // checken was ist wenn 0 drin ist
+  //const path = L.polyline(points, { className: 'tracking_line', snakingSpeed: 500 });
 
-    const path = L.motion.polyline(points, {
-        className: 'tracking_line',
-    }, {
+  const path = L.motion
+    .polyline(
+      points,
+      {
+        className: "tracking_line",
+      },
+      {
         easing: L.Motion.Ease.easeInOutQuart,
         auto: true,
         duration: 2000,
-    }, {
+      },
+      {
         removeOnEnd: false,
         icon: L.divIcon({
-            html: "<div class='material-icons trackingIcon'>🚚</div>",
-            iconSize: L.point(0, 0)
-        })
-    }).addTo(map);
+          html: "<div class='material-icons trackingIcon'>🚚</div>",
+          iconSize: L.point(0, 0),
+        }),
+      }
+    )
+    .addTo(map);
 
-    map.addLayer(path);
-    mapItems.push(path);
+  map.addLayer(path);
+  mapItems.push(path);
 
-    var start = history[0]; // lila
-    var current = history[history.length - 1]; // grün
+  var start = history[0]; // lila
+  var current = history[history.length - 1]; // grün
 
-    var greenIcon = new L.Icon({
-        iconUrl: 'https://cdn.rawgit.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-green.png',
-        shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
-        iconSize: [25, 41],
-        iconAnchor: [12, 41],
-        popupAnchor: [1, -34],
-        shadowSize: [41, 41]
-    });
+  var greenIcon = new L.Icon({
+    iconUrl:
+      "https://cdn.rawgit.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-green.png",
+    shadowUrl:
+      "https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png",
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+    shadowSize: [41, 41],
+  });
 
-    var violetIcon = new L.Icon({
-        iconUrl: 'https://cdn.rawgit.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-violet.png',
-        shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
-        iconSize: [25, 41],
-        iconAnchor: [12, 41],
-        popupAnchor: [1, -34],
-        shadowSize: [41, 41]
-    });
+  var violetIcon = new L.Icon({
+    iconUrl:
+      "https://cdn.rawgit.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-violet.png",
+    shadowUrl:
+      "https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png",
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+    shadowSize: [41, 41],
+  });
 
-    points.forEach((point, i) => {
-        const marker = L.marker(point, { autoPan: true });
+  points.forEach((point, i) => {
+    const marker = L.marker(point, { autoPan: true });
 
-        const entry = history[i];
+    const entry = history[i];
 
-        // ugly af monkaS
-        var test = history.filter(e => e.location == entry.location && e != entry);
+    // ugly af monkaS
+    var test = history.filter(
+      (e) => e.location == entry.location && e != entry
+    );
 
-        if (test.every(t => t.updatedAt < entry.updatedAt)) {
-            //entry.lat + " " + " " + entry.long + " " + 
-            marker.bindTooltip(entry.location + " <br> " + formatDateTime(entry.updatedAt), {
-                permanent: true,
-            }).openTooltip();
+    if (test.every((t) => t.updatedAt < entry.updatedAt)) {
+      //entry.lat + " " + " " + entry.long + " " +
+      marker
+        .bindTooltip(
+          entry.location + " <br> " + formatDateTime(entry.updatedAt),
+          {
+            permanent: true,
+          }
+        )
+        .openTooltip();
 
-            if (start == entry) {
-                marker.setIcon(violetIcon);
-            }
+      if (start == entry) {
+        marker.setIcon(violetIcon);
+      }
 
-            if (current == entry) {
-                marker.setIcon(greenIcon);
-            }
+      if (current == entry) {
+        marker.setIcon(greenIcon);
+      }
 
-            map.addLayer(marker);
-            mapItems.push(marker);
-        }
-    });
+      map.addLayer(marker);
+      mapItems.push(marker);
+    }
+  });
 }
 
 function formatDateTime(date_string) {
-    return new Date(date_string).toLocaleString();
+  return new Date(date_string).toLocaleString();
 }
