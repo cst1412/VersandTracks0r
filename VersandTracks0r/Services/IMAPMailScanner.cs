a0d8a9961e3b5a298fa6488fce455115bff0ae3a
chris <chrismaifeld@gmail.com>
2020-08-19 20:21:21 +0200

    EmailScanner durchsucht nun auch Links von Mails
    EmailScanner durchsucht nun auch Links von Mails

--- VersandTracks0r/Services/IMAPMailScanner.cs
+++ VersandTracks0r/Services/IMAPMailScanner.cs
@@ -1,4 +1,5 @@
-﻿using MailKit;
+﻿using HtmlAgilityPack;
+using MailKit;
 using MailKit.Net.Imap;
 using MailKit.Search;
 using MimeKit;
@@ -6,6 +7,7 @@ using System;
 using System.IO;
 using System.Linq;
 using System.Text.RegularExpressions;
+using System.Xml.Linq;
 using VersandTracks0r.Models;
 
 namespace VersandTracks0r.Services
@@ -13,12 +15,13 @@ namespace VersandTracks0r.Services
     public class IMAPMailScanner
     {
         private readonly Regex dhl = new Regex(@"(\d{20}|JD\d{18}|JJD\d{18})");
+        private readonly Regex dhlDetailRegex = new Regex(@"(\d{12}|\d{20}|JD\d{18}|JJD\d{18})");
         private readonly Regex ups = new Regex(@"(1Z\w{16})");
         private readonly Regex amazon = new Regex(@"(A\w{2}\d{9})", RegexOptions.IgnoreCase);
         private readonly Regex dpd = new Regex(@"(0\d{13})");
         private readonly Regex hermes = new Regex(@"(\d{14})");
         private readonly AppSettings appSettings;
-        
+
         private DateTime lastScan = DateTime.Today.Subtract(TimeSpan.FromDays(30));
 
         public event Action<Shipment> Found;
@@ -43,7 +46,7 @@ namespace VersandTracks0r.Services
 
             var inbox = client.Inbox;
             inbox.Open(FolderAccess.ReadOnly);
-            
+
             var result = inbox.Search(SearchQuery.DeliveredAfter(this.lastScan));
             this.lastScan = DateTime.Now.Subtract(TimeSpan.FromMinutes(5));
 
@@ -58,54 +61,122 @@ namespace VersandTracks0r.Services
 
         public void Parse(MimeMessage message)
         {
-            if (message.TextBody is string text)
+            if (message.TextBody is string || message.HtmlBody is string)
             {
+                string text = message.TextBody;
+                if (string.IsNullOrWhiteSpace(text))
+                {
+                    text = message.HtmlBody;
+                }
                 text = text.ToLower();
                 var address = message.From.Mailboxes.First().Address;
                 var domain = address.Split('@').Last();
 
-                // if (!text.Contains(this.appSettings.Name.ToLower()))
-                // {
-                //     return;
-                // }
+                if (message.Subject.Contains("MediaMarkt"))
+                {
+                    var d = "";
+                }
 
-                var shipment = new Shipment
+                if (this.checkMatchesForCarriers(text, domain))
                 {
-                    CreatedAt = DateTime.Now,
-                    Comment = domain,
-                };
+                    return;
+                }
 
-                var test = this.dhl.Matches(text);
-                if (this.dhl.Match(text) is Match dhlMatch && dhlMatch.Success && text.Contains("dhl"))
+                Regex regex = new Regex("<a\\s+(?:[^>]*?\\s+)?href=([\"'])(.*?)\\1", RegexOptions.IgnoreCase);
+                Match match;
+                for (match = regex.Match(text); match.Success; match = match.NextMatch())
                 {
-                    shipment.Carrier = Carrier.DHL;
-                    shipment.TrackingId = dhlMatch.Groups[0].Value;
-                    this.Found?.Invoke(shipment);
+                    Console.WriteLine("Found a href. Groups: ");
+                    foreach (Group group in match.Groups)
+                    {
+                        Console.WriteLine(group.Value);
+                        var doc = new HtmlDocument();
+                        doc.LoadHtml(group.Value);
+                        var anchor = doc.DocumentNode.FirstChild;
+                        string href ="";
+                        if (anchor != null && anchor.Attributes.Count > 0 && anchor.Attributes["href"] != null)
+                        {
+                            href = anchor.Attributes["href"].Value;
+                        }
+                        if (!string.IsNullOrWhiteSpace(href))
+                        {
+                            Uri url;
+                            bool parseResult = Uri.TryCreate(href, UriKind.RelativeOrAbsolute, out url);
+                            if (parseResult)
+                            {
+                                Console.WriteLine(href);
+                                if (this.checkMatchesForCarriers(url.ToString(), domain, true))
+                                {
+                                    return;
+                                }
+                            }
+                        }
+                    }
                 }
-                else if (this.ups.Match(text) is Match upsMatch && upsMatch.Success && text.Contains("ups"))
-                {
-                    shipment.Carrier = Carrier.UPS;
-                    shipment.TrackingId = upsMatch.Groups[0].Value;
-                    this.Found?.Invoke(shipment);
-                }
-                else if (this.dpd.Match(text) is Match dpdMatch && dpdMatch.Success && text.Contains("dpd"))
-                {
-                    shipment.Carrier = Carrier.DPD;
-                    shipment.TrackingId = dpdMatch.Groups[0].Value;
-                    this.Found?.Invoke(shipment);
-                }
-                else if (this.hermes.Match(text) is Match hermesMatch && hermesMatch.Success && text.Contains("hermes"))
-                {
-                    shipment.Carrier = Carrier.Hermes;
-                    shipment.TrackingId = hermesMatch.Groups[0].Value;
-                    this.Found?.Invoke(shipment);
-                }
-                else if (this.amazon.Match(text) is Match amazonMatch && amazonMatch.Success && text.Contains("amazon"))
-                {
-                    shipment.Carrier = Carrier.Amazon;
-                    shipment.TrackingId = amazonMatch.Groups[0].Value;
-                    this.Found?.Invoke(shipment);
-                }
+
+
+            }
+        }
+
+        private bool checkMatchesForCarriers(string text, string domain, bool useDetailRegex = false)
+        {
+            var shipment = new Shipment
+            {
+                CreatedAt = DateTime.Now,
+                Comment = domain,
+            };
+
+            var test = this.dhl.Matches(text);
+            if (this.dhl.Match(text) is Match dhlMatch && dhlMatch.Success && text.Contains("dhl"))
+            {
+                shipment.Carrier = Carrier.DHL;
+                shipment.TrackingId = dhlMatch.Groups[0].Value;
+                this.Found?.Invoke(shipment);
+                return true;
+            }
+            else if (this.ups.Match(text) is Match upsMatch && upsMatch.Success && text.Contains("ups"))
+            {
+                shipment.Carrier = Carrier.UPS;
+                shipment.TrackingId = upsMatch.Groups[0].Value;
+                this.Found?.Invoke(shipment);
+                return true;
+
+            }
+            else if (this.dpd.Match(text) is Match dpdMatch && dpdMatch.Success && text.Contains("dpd"))
+            {
+                shipment.Carrier = Carrier.DPD;
+                shipment.TrackingId = dpdMatch.Groups[0].Value;
+                this.Found?.Invoke(shipment);
+                return true;
+
+            }
+            else if (this.hermes.Match(text) is Match hermesMatch && hermesMatch.Success && text.Contains("hermes"))
+            {
+                shipment.Carrier = Carrier.Hermes;
+                shipment.TrackingId = hermesMatch.Groups[0].Value;
+                this.Found?.Invoke(shipment);
+                return true;
+
+            }
+            else if (this.amazon.Match(text) is Match amazonMatch && amazonMatch.Success && text.Contains("amazon"))
+            {
+                shipment.Carrier = Carrier.Amazon;
+                shipment.TrackingId = amazonMatch.Groups[0].Value;
+                this.Found?.Invoke(shipment);
+                return true;
+
+            }
+            else if (useDetailRegex && this.dhlDetailRegex.Match(text) is Match dhlDetailRegex && dhlDetailRegex.Success
+                    && (text.Contains("sendung") || text.Contains("verfolgen") || text.Contains("track")))
+            {
+                shipment.Carrier = Carrier.DHL;
+                shipment.TrackingId = dhlDetailRegex.Groups[0].Value;
+                this.Found?.Invoke(shipment);
+                return true;
+            }
+            else
+            {
+                return false;
             }
         }
     }
